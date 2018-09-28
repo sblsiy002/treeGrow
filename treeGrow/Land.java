cf8b4e8f17c1d67fb66fa350c168897ddafd9b81
sblsiy002 <43615505+sblsiy002@users.noreply.github.com>
2018-09-28 22:07:46 +0200

    Add files via upload

---
+++ treeGrow/Land.java
@@ -0,0 +1,59 @@
+package treeGrow;
+
+public class Land{
+	
+	// to do
+   int x=0;
+   int y=0;
+	// sun exposure data here
+
+   static float shadefraction = 0.1f; // only this fraction of light is transmitted by a tree
+
+   Land(int dx, int dy) {
+      this.x=dx;
+      this.y=dy;
+   }
+
+	// return the number of landscape cells in the x dimension
+   int getDimX() {
+      return this.x;	}
+	
+	// return the number of landscape cells in the y dimension
+   int getDimY() {
+   	  return this.y; // incorrect value
+   }
+	
+	// Reset the shaded landscape to the same as the initial sun exposed landscape
+	// Needs to be done after each growth pass of the simulator
+   void resetShade() {
+   	// to do
+   }
+	
+	// return the sun exposure of the initial unshaded landscape at position <x,y?
+   float getFull(int x, int y) {
+   	// to do
+      return 0.0f; // incorrect value
+   }
+	
+	// set the sun exposure of the initial unshaded landscape at position <x,y> to <val>
+   void setFull(int x, int y, float val) {
+   	// to do 
+   }
+	
+	// return the current sun exposure of the shaded landscape at position <x,y>
+   float getShade(int x, int y) {
+   	// to do 
+      return 0.0f; // incorrect value
+   }
+	
+	// set the sun exposure of the shaded landscape at position <x,y> to <val>
+   void setShade(int x, int y, float val){
+   	// to do
+   }
+	
+	// reduce the sun exposure of the shaded landscape to 10% of the original
+	// within the extent of <tree>
+   void shadow(Tree tree){
+   	// to do
+   }
+}
