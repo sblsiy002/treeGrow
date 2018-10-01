77e033ef2bce6ec6a22cbd2d3a773de4db54f178
sblsiy002 <43615505+sblsiy002@users.noreply.github.com>
2018-10-01 19:36:55 +0200

    Add files via upload

--- Land.java
+++ Land.java
@@ -1,11 +1,17 @@
-package treeGrow;
 
+package treeGrow;
+import java.util.*;
 public class Land{
 	
 	// to do
    int x=0;
    int y=0;
+   float v=0.0f;
    SunData sunData;
+   List<Integer> Arrx=new ArrayList<Integer>();
+   List<Integer> Arry=new ArrayList<Integer>();
+   List<Float> ArrV=new ArrayList<Float>();
+
 	// sun exposure data here
 
    static float shadefraction = 0.1f; // only this fraction of light is transmitted by a tree
@@ -21,37 +27,70 @@ public class Land{
 	
 	// return the number of landscape cells in the y dimension
    int getDimY() {
-   	  return this.y;   }
+      return this.y;   }
 	
 	// Reset the shaded landscape to the same as the initial sun exposed landscape
 	// Needs to be done after each growth pass of the simulator
    void resetShade() {
    	// to do
-        }
+   }
 	
 	// return the sun exposure of the initial unshaded landscape at position <x,y?
    float getFull(int x, int y) {
-   	// to do
-      return 3.0f; // incorrect value
+   	
+      synchronized(Arrx){
+         Iterator<Integer> itr=Arrx.iterator();
+         Iterator<Integer> itr1=Arry.iterator();
+         Iterator<Float> itr2=ArrV.iterator();
+         while(itr.hasNext()&&itr1.hasNext()&&itr2.hasNext()){
+            if(itr.next()==x && itr1.next()==y){
+               v=itr2.next(); 
+            }
+         
+         }
+      }
+   
+      return v;// incorrect value
+   
    }
+         
 	
 	// set the sun exposure of the initial unshaded landscape at position <x,y> to <val>
    void setFull(int x, int y, float val) {
-   	   this.x=x;
-         this.y=y;
-         
-         }
+      
+      Arrx.add(x);
+      Arry.add(y);
+      ArrV.add(val);
+     // Arrx=Collections.synchronizedList(Arrx);
+     // Arry=Collections.synchronizedList(Arry);
+     // ArrV=Collections.synchronizedList(ArrV);
+   
+   }
 	
 	// return the current sun exposure of the shaded landscape at position <x,y>
    float getShade(int x, int y) {
-   	// to do 
-      return 3.0f; // incorrect value
+      return 0.1f*getFull(x,y); // incorrect value
    }
 	
 	// set the sun exposure of the shaded landscape at position <x,y> to <val>
    void setShade(int x, int y, float val){
-   	// to do
-      
+   	
+      synchronized(Arrx){
+         Iterator<Integer> itr=Arrx.iterator();
+         Iterator<Integer> itr1=Arry.iterator();
+         Iterator<Float> itr2=ArrV.iterator();
+         while(itr.hasNext()&&itr1.hasNext()&&itr2.hasNext()){
+            if(itr.next()==x && itr1.next()==y){
+               itr.remove();
+               itr1.remove();
+               itr2.remove();
+                Arrx.add(x);
+                Arry.add(y);
+                ArrV.add(val);
+            }
+         
+         }
+      }
    }
 	
 	// reduce the sun exposure of the shaded landscape to 10% of the original
--- TreeGrow.java
+++ TreeGrow.java
@@ -8,67 +8,121 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 public class TreeGrow {
-	static long startTime = 0;
-	static int frameX;
-	static int frameY;
-	static ForestPanel fp;
+   static long startTime = 0;
+   static int frameX;
+   static int frameY;
+   static String d="";
+   static ForestPanel fp;
 
 	// start timer
-	private static void tick(){
-		startTime = System.currentTimeMillis();
-	}
+   private static void tick(){
+      startTime = System.currentTimeMillis();
+   }
 	
 	// stop timer, return time elapsed in seconds
-	private static float tock(){
-		return (System.currentTimeMillis() - startTime) / 1000.0f; 
-	}
+   private static float tock(){
+      return (System.currentTimeMillis() - startTime) / 1000.0f; 
+   }
 	
-	public static void setupGUI(int frameX,int frameY,Tree [] trees) {
-		Dimension fsize = new Dimension(800, 800);
-		// Frame init and dimensions
-    	JFrame frame = new JFrame("Photosynthesis"); 
-    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    	frame.setPreferredSize(fsize);
-    	frame.setSize(800, 800);
-    	
-      	JPanel g = new JPanel();
-        g.setLayout(new BoxLayout(g, BoxLayout.PAGE_AXIS)); 
-      	g.setPreferredSize(fsize);
- 
-		fp = new ForestPanel(trees);
-		fp.setPreferredSize(new Dimension(frameX,frameY));
-		JScrollPane scrollFrame = new JScrollPane(fp);
-		fp.setAutoscrolls(true);
-		scrollFrame.setPreferredSize(fsize);
-	    g.add(scrollFrame);
-    	
-      	frame.setLocationRelativeTo(null);  // Center window on screen.
-      	frame.add(g); //add contents to window
-        frame.setContentPane(g);     
-        frame.setVisible(true);
-        Thread fpt = new Thread(fp);
-        fpt.start();
-	}
+   public static void setupGUI(int frameX,int frameY,Tree [] trees) {
+      Dimension fsize = new Dimension(800, 800);
+   	// Frame init and dimensions
+      JFrame frame = new JFrame("Photosynthesis"); 
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setPreferredSize(fsize);
+      frame.setSize(800, 800);
+      JButton btnPlay=new JButton("Play");
+      JButton btnReset=new JButton("Reset");
+      JButton btnEnd=new JButton("End");
+      JButton btnPause=new JButton("Pause");
+      btnPlay.setSize(100, 30);
+      btnReset.setSize(100, 30);
+      btnEnd.setSize(100, 30);
+      btnPause.setSize(100, 30);
+      JPanel p1 = new JPanel();
+      p1.add(btnReset);
+      p1.add(Box.createRigidArea(new Dimension(15,15)));
+      p1.add(btnPause);
+      p1.add(Box.createRigidArea(new Dimension(15,0)));
+      p1.add(btnPlay);
+      p1.add(Box.createRigidArea(new Dimension(15,0)));
+      p1.add(btnEnd);
+       float count=1;
+      btnEnd.addActionListener(
+            new java.awt.event.ActionListener() {
+               public void actionPerformed(java.awt.event.ActionEvent evt) {
+                  System.exit(0);         
+               }
+            });
+      
+      btnReset.addActionListener(
+            new java.awt.event.ActionListener() {
+               public void actionPerformed(java.awt.event.ActionEvent evt) {
+                  
+                  for(int t = 0; t < trees.length; t++){  
+                   trees[t].setExt(1);               
+                   }                      
+               }
+            });
+       
+
+   
+      p1.setLayout(new BoxLayout(p1, BoxLayout.X_AXIS)); 
+      	
+   
+      JPanel g = new JPanel();
+      g.setLayout(new BoxLayout(g, BoxLayout.PAGE_AXIS)); 
+      g.setPreferredSize(fsize);
+   
+      fp = new ForestPanel(trees);
+      fp.setPreferredSize(new Dimension(frameX,frameY));
+      JScrollPane scrollFrame = new JScrollPane(fp);
+      fp.setAutoscrolls(true);
+      scrollFrame.setPreferredSize(fsize);
+      g.add(scrollFrame);
+      frame.setLocationRelativeTo(null);  // Center window on screen.
+      frame.add(g); //add contents to window
+      frame.setContentPane(g);
+         
+        
+         
+      frame.add(p1);
+   
+   
+      frame.setVisible(true);
+      Thread fpt = new Thread(fp);
+      fpt.start();
+       btnPlay.addActionListener(
+            new java.awt.event.ActionListener() {
+               public void actionPerformed(java.awt.event.ActionEvent evt) {
+                  for(int t = 0; t < trees.length; t++){  
+                   trees[t].setExt(trees[t].getExt()+0.2f );
+                  }
+               }
+              
+              });
+        
+   }
 	
 		
-	public static void main(String[] args) {
-	 SunData sundata = new SunData();
-		
-		// check that number of command line arguments is correct
-		if(args.length != 1)
-		{
-			System.out.println("Incorrect number of command line arguments. Should have form: java treeGrow.java intputfilename");
-			System.exit(0);
-		}
-				
-		// read in forest and landscape information from file supplied as argument
-		sundata.readData(args[0]);
-		System.out.println("Data loaded");
-		
-		frameX = sundata.sunmap.getDimX();
-	   frameY = sundata.sunmap.getDimY();
-		setupGUI(frameX, frameY, sundata.trees);
-		
-		// create and start simulation loop here as separate thread
-	}
+   public static void main(String[] args) {
+      SunData sundata = new SunData();
+   	
+   	// check that number of command line arguments is correct
+      if(args.length != 1)
+      {
+         System.out.println("Incorrect number of command line arguments. Should have form: java treeGrow.java intputfilename");
+         System.exit(0);
+      }
+   			
+   	// read in forest and landscape information from file supplied as argument
+      sundata.readData(args[0]);
+      System.out.println("Data loaded");
+   	d=args[0];
+      frameX = sundata.sunmap.getDimX();
+      frameY = sundata.sunmap.getDimY();
+      setupGUI(frameX, frameY, sundata.trees);
+   	
+   	// create and start simulation loop here as separate thread
+   }
 }
